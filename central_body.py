0b8c10c700a542bf8cf752cec7f0b3d35a1d2ebf
Jasmeet Singh <jasmeet0915@gmail.com>
2020-05-24 18:48:35 +0530

    plotting for central body added

--- central_body.py
+++ central_body.py
@@ -1,10 +1,12 @@
+import numpy as np
+
 # Universal Gravitational Constant
 G_m = 6.67408e-11
 G_km = G_m*10**-9
 
 
 class CentralBody:
-    def __init__(self, name, mass, radius, tilt, axis_tilt, rotation_period):
+    def __init__(self, name, mass, radius, tilt, axis_tilt=0, rotation_period=0):
         self.name = name
         self.mass = mass
         self.radius = radius
@@ -13,3 +15,12 @@ class CentralBody:
         self.rotation_period = rotation_period
 
         self.mu = G_km * self.mass
+
+    def plot_attributes(self):
+        # plot central_body using polar coordinates of sphere
+        _u, _v = np.mgrid[0:2 * np.pi:20j, 0:np.pi:10j]
+        _x = self.radius * np.cos(_u) * np.sin(_v)
+        _y = self.radius * np.sin(_u) * np.sin(_v)
+        _z = self.radius * np.cos(_v)
+
+
