5836cae0ecf00d24bfacf2cd6c13dc69d887129f
Jasmeet Singh <jasmeet0915@gmail.com>
2020-05-25 03:03:36 +0530

    satellite class added

---
+++ satellite.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class Satellite:
+    def __init__(self, sat_id, name, center_body, sat_type)
+        self.sat_id = sat_id
+        self.name = name
+        self.sat_type = sat_type
+        self.center_body = center_body
+
+
+    def propagate_with_coes(self, e, a, inclination, ):
+        pass
